9fbe091c330b0345ffc13fd62ab6f63c2e2c7d22
123ahmed565 <ahmedseragmohamed@gmail.com>
2022-03-18 19:01:47 +0200

    [UPLOAD]: firts upload

---
+++ js/main.js
@@ -0,0 +1,45 @@
+// modal search popup for mobile
+$('a[href="#search"]').click(function () {
+    event.preventDefault();
+    $("#search-box").addClass("-open");
+    setTimeout(function () {
+      inputSearch.focus();
+    }, 800);
+});
+
+$('a[href="#close"]').click(function () {
+    event.preventDefault();
+    $("#search-box").removeClass("-open");
+});
+
+$(document).keyup(function (e) {
+    if (e.keyCode == 27) {
+      // escape key maps to keycode `27`
+      $("#search-box").removeClass("-open");
+    }
+});
+
+// swiper mini slider
+var swiper = new Swiper(".mySwiper2", {
+    slidesPerView: 6.5,
+    spaceBetween: 5,
+    freeMode: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints:{
+        0: {
+            slidesPerView: 3.5,
+            spaceBetween: 10,
+        },
+        600: {
+            slidesPerView: 5.5,
+            spaceBetween: 20,
+        },
+        1000: {
+            slidesPerView: 6.5,
+            spaceBetween: 10,
+        },
+    }
+});
